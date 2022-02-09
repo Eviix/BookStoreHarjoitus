5eed89bcd2e22632d6c109d95d0e70f9323017ed
Eviix <daniel.ristikari@gmail.com>
2022-02-09 15:25:41 +0200

    ekat lisäykset

---
+++ BookstoreProjekti/src/main/java/hh/swd20/BookstoreProjekti/BookstoreProjektiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.BookstoreProjekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreProjektiApplication.class, args);
+	}
+
+}
